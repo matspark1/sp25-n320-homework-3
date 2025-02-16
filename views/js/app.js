1f353d509aceb45a2aa235a43213a4384346934c
Matthew Sparkman <122379993+matspark1@users.noreply.github.com>
2025-02-16 02:46:40 -0500

    finishing touches

--- views/js/app.js
+++ views/js/app.js
@@ -1,5 +1,17 @@
 const submitBtn = document.querySelector(".submitBtn");
 
-submitBtn.addEventListener("click", () => {
-  alert("Your message has been sent! We'll get back to you shortly.");
-});
+if (submitBtn) {
+  submitBtn.addEventListener("click", () => {
+    alert("Your message has been sent! We'll get back to you shortly.");
+  });
+}
+
+const bg = document.querySelector(".bg");
+const body = document.body;
+
+if (bg) {
+  bg.addEventListener("click", () => {
+    console.log("He213");
+    body.classList.toggle("primary");
+  });
+}
